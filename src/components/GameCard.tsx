ed111453c69c9c82bba3eaffc80efe5262a1408e
zhangyule-six <1216111921@qq.com>
2025-03-08 16:50:56 +0800

    show loading skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="250px" borderRadius={10} overflow="hidden">
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>

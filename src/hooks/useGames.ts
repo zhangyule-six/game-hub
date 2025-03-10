83c37b6ce34811467fad4e6063888a8f3a9b055d
zhangyule-six <1216111921@qq.com>
2025-03-10 20:56:38 +0800

    add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ import { Genre } from "./useGenres";
   background_image: string;
   parent_platforms:{platform:Platform}[];
   metacritic:number;
+  rating_top: number;
 }
 
 const useGames =(gameQuery:GameQuery) =>
@@ -22,6 +23,7 @@ const useGames =(gameQuery:GameQuery) =>
       platforms:gameQuery.platform?.id,
       ordering:gameQuery.sortOrder,
       search: gameQuery.searchText
+
     }},
     [gameQuery ])
 export default useGames;

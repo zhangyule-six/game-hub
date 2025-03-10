9339b9ee4f07bb6f06bec4c46dee3f536a9aa4b3
zhangyule-six <1216111921@qq.com>
2025-03-10 20:40:39 +0800

    Clean up game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,15 +11,15 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={getCroppedImageUrl(game.background_image)}></Image>
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );

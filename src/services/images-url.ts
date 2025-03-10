2c67a1030ea2fde842126ea1e611d656b1e42cd8
zhangyule-six <1216111921@qq.com>
2025-03-10 15:33:32 +0800

    Show a spinner while  fetching the genres

--- src/services/images-url.ts
+++ src/services/images-url.ts
@@ -1,9 +1,8 @@
 
-
 const getCroppedImageUrl = (url:string) =>{
   const target = 'media/';
   const index = url.indexOf(target)+ target.length;//为了包括‘media/’本身的长度
- return url.slice(0, index) + 'crop/600/400' + url.slice(index);
+ return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 
 }
-export default getCroppedImageUrl
+export default getCroppedImageUrl;
